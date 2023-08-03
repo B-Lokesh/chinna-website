ae45b2b9bbcb5d596476a3acd6f01ced578c4225
BATCHU LOKESH <84063366+B-Lokesh@users.noreply.github.com>
2023-08-03 17:54:09 +0530

    Add files via upload

---
+++ lesson-11 tasks/11x.js
@@ -0,0 +1,49 @@
+let arrMy =JSON.parse(localStorage.getItem('arrMy')) || [{
+  name: 'make dinner',
+  dueDate: '2023-07-07'
+}, {
+  name: 'wash dishes',
+  dueDate: '2023-07-07'
+}];
+renderTodoLIst();
+localStorage.setItem('arrMy',JSON.stringify(arrMy));
+function renderTodoLIst(){
+
+      let todolistHtml = '';
+      for(let i = 0; i < arrMy.length; i++)
+      {
+            const todo = arrMy[i]; 
+            let name = todo.name;
+            let dueDate = todo.dueDate;
+            let html = 
+            `<div>
+            ${name} </div>
+            <div>${dueDate}</div>
+           <div> <button onclick="
+                  arrMy.splice(${i},1);
+                  renderTodoLIst();/*again display updated list*/
+                localStorage.setItem('arrMy',JSON.stringify(arrMy));
+            " class="delete-todo-button">Delete</button>
+            </div>`;
+
+            /*bcz div doesnt add space b/w lines and coming to <p> inside one <p> other <p> not possible so div used.shortcut generating htmlk instread of writing unlimited <p>'s.*/
+            todolistHtml += html;
+      }
+      console.log(todolistHtml);
+
+      document.querySelector('.js-todo-list')
+      .innerHTML = todolistHtml;
+}
+function funtodo(){
+      const innerElement=document.querySelector('.js-todo');
+      let element = innerElement.value;
+      let dateInputElement = document.querySelector('.js-due-date-input');
+      let dueDate = dateInputElement.value;
+       arrMy.push({name : element,
+       dueDate : dueDate});
+      console.log(arrMy);
+      innerElement.value = '';//to empty textbox after adding
+
+      renderTodoLIst();
+      localStorage.setItem('arrMy',JSON.stringify(arrMy));
+}
